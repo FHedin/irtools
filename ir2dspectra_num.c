d15e36e72dd60b12c162da0e4aac4bc4794e4509
cazade <pierre.cazade@unibas.ch>
2014-07-28 11:44:26 +0100

    First commit

---
+++ ir2dspectra_num.c
@@ -0,0 +1,284 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <complex.h>
+#include <fftw3.h>
+
+#define clight 299792458
+#define PI 3.14159265358979323846
+
+int nls=0;
+
+double* ls=NULL;
+double dtau=0.005;
+double wm,delta;
+double cmtops=2*PI*clight*1.e-10;
+
+void integrate_ls(FILE* inp);
+double g(double t);
+double complex Rr(double t1 ,double t2, double t3);
+double complex Rnr(double t1 ,double t2, double t3);
+
+int main(int argc, char* argv[])
+{
+  FILE* input=fopen(argv[1],"r");
+  FILE* out=fopen("spec.dat","w");
+  
+  FILE* input2=NULL;
+  
+  char namei[256];
+  
+  int i,j,k,kk,l,ll,ndata;
+  double t1,t2,t3,dt;
+  double w1min,w1max,w3min,w3max;
+  
+  fftw_complex *rlist,*ftrlist,*rnlist,*ftrnlist;
+  fftw_plan pr,prn;
+  
+  fscanf(input,"%s",namei);
+  fscanf(input,"%lf",&dtau);
+  fscanf(input,"%lf %lf",&wm,&delta);
+  fscanf(input,"%d %lf",&ndata,&dt);
+  fscanf(input,"%lf %lf %lf %lf",&w1min,&w1max,&w3min,&w3max);
+  fscanf(input,"%lf",&t2);
+  
+  rlist=(fftw_complex*) fftw_malloc(ndata*ndata*sizeof(*rlist));
+  ftrlist=(fftw_complex*) fftw_malloc(ndata*ndata*sizeof(*ftrlist));
+  rnlist=(fftw_complex*) fftw_malloc(ndata*ndata*sizeof(*rnlist));
+  ftrnlist=(fftw_complex*) fftw_malloc(ndata*ndata*sizeof(*ftrnlist));
+  
+  pr=fftw_plan_dft_2d(ndata, ndata, rlist,ftrlist , FFTW_BACKWARD, FFTW_ESTIMATE);
+  prn=fftw_plan_dft_2d(ndata, ndata, rnlist,ftrnlist , FFTW_BACKWARD, FFTW_ESTIMATE);
+  
+  input2=fopen(namei,"r");
+  integrate_ls(input2);
+  fclose(input2);
+    
+  k=0;
+  for(i=0;i<ndata;i++)
+  {
+    
+    t1=(double)i*dt;
+    
+    for(j=0;j<ndata;j++)
+    {
+      
+      t3=(double)j*dt;
+      
+      rlist[k]=Rr(t1,t2,t3);
+      rnlist[k]=Rnr(t1,t2,t3);
+      
+      k++;
+    }
+    
+  }
+  
+  for(i=0;i<ndata;i++)
+  {
+      
+    rlist[i]=0.5*rlist[i];
+    rnlist[i]=0.5*rnlist[i];
+    
+  }
+  
+  for(i=1;i<ndata;i++)
+  {
+    k=i*ndata;
+    rlist[k]=0.5*rlist[k];
+    rnlist[k]=0.5*rnlist[k];
+    
+  }
+  
+  fftw_execute(pr);
+  fftw_execute(prn);
+  
+  for(i=0;i<ndata/2;i++)
+  {
+    
+    for(j=0;j<ndata/2;j++)
+    {
+        
+      k=i*ndata+j;
+      kk=i*ndata+j+ndata/2;
+      l=(i+ndata/2)*ndata+j;
+      ll=(i+ndata/2)*ndata+j+ndata/2;
+      
+      rlist[l]=ftrlist[kk];
+      rlist[ll]=ftrlist[k];
+      rlist[k]=ftrlist[ll];
+      rlist[kk]=ftrlist[l];
+      
+      rnlist[l]=ftrnlist[kk];
+      rnlist[ll]=ftrnlist[k];
+      rnlist[k]=ftrnlist[ll];
+      rnlist[kk]=ftrnlist[l];
+      
+      
+    }
+    
+  }
+  
+  fftw_free(ftrlist);
+  fftw_free(ftrnlist);
+  
+  ftrlist=(fftw_complex*) fftw_malloc((ndata-1)*(ndata-1)*sizeof(*ftrlist));
+  ftrnlist=(fftw_complex*) fftw_malloc((ndata-1)*(ndata-1)*sizeof(*ftrnlist));
+  
+  k=0;
+  for(i=1;i<ndata;i++)
+  {
+    
+    for(j=1;j<ndata;j++)
+    {
+      
+      l=ndata*i+j;
+      ll=ndata*(ndata-i)+j;
+      
+      ftrlist[k]=rlist[ll];
+      ftrnlist[k]=rnlist[l];
+      
+      k++;
+    }
+    
+  }
+  
+  double w1,w3,res;
+  
+  k=0;
+  for(i=ndata/2-1;i<ndata-1;i++)
+  {
+    
+    w1=1.e10/((double)ndata*clight*dt)*(double)(1-ndata/2+i);
+    
+    if(w1>w1max)
+      break;
+    
+    if( w1>=w1min )
+    {
+      
+      for(j=ndata/2-1;j<ndata-1;j++)
+      {
+	
+	k=i*(ndata-1)+j;
+	w3=1.e10/((double)ndata*clight*dt)*(double)(1-ndata/2+j);
+	
+	if(w3>w3max)
+	  break;
+	
+	if( w3>=w3min )
+	{
+	  
+	  res=creal(ftrlist[k]+ftrnlist[k])/(double)ndata;
+	
+	  fprintf(out,"%lf\t%lf\t%le\n",w1,w3,res);
+	  
+	}
+	
+      }
+      
+      fprintf(out,"\n");
+      
+    }
+    
+  }
+  
+  fftw_destroy_plan(pr);
+  fftw_destroy_plan(prn);
+  
+  fftw_free(rlist);
+  fftw_free(ftrlist);
+  fftw_free(rnlist);
+  fftw_free(ftrnlist);
+  
+  fclose(out);
+  fclose(input);
+  
+  return 0;
+  
+}
+
+void integrate_ls(FILE* inp)
+{
+  char buf[4];
+  
+  int i,idx;
+  
+  double fmean,cn;
+  
+  fscanf(inp,"%s %d %lf",buf,&nls,&fmean);
+  
+  ls=(double*)malloc(nls*sizeof(double));
+  
+  for(i=0;i<nls;i++)
+  {
+    fscanf(inp,"%d %lf %lf",&idx,(ls+i),&cn);
+  }
+  
+  // First integration
+  
+  for(i=(nls-1);i>0;i--)
+  {
+    ls[i]=0.5*(ls[i]+ls[i-1])*dtau;
+  }
+  
+  ls[0]=0.;
+  for(i=1;i<nls;i++)
+  {
+    ls[i]=ls[i]+ls[i-1];
+  }
+  
+  // Second integration
+  
+  for(i=(nls-1);i>0;i--)
+  {
+    ls[i]=0.5*(ls[i]+ls[i-1])*dtau;
+  }
+  
+  ls[0]=0.;
+  for(i=1;i<nls;i++)
+  {
+    ls[i]=ls[i]+ls[i-1];
+    //printf("%d %lf\n",i,ls[i]);
+  }
+  
+}
+
+double g(double t)
+{
+  
+  int i;
+  double tdt,ya,yb,val;
+  
+  tdt=t/dtau;
+  i=(int)tdt;
+  
+  ya=ls[i];
+  yb=ls[i+1];
+  
+  val=ya+(yb-ya)*(tdt-(double)i);
+  
+  return ( val );
+  
+}
+
+double complex Rr(double t1 ,double t2, double t3)
+{
+  double complex f1;
+  double f2;
+  
+  f1 = cexp( -I * ( t3 - t1 ) * wm * cmtops ) - cexp( -I * cmtops * ( ( wm - delta ) * t3 - ( wm * t1 ) ) );
+  f2 = exp( -g(t1) + g(t2) - g(t3) - g(t1+t2) - g(t2+t3) + g(t1+t2+t3) );
+  
+  return ( f1 * f2 );
+}
+
+double complex Rnr(double t1 ,double t2, double t3)
+{
+  double complex f1;
+  double f2;
+  
+  f1 = cexp( -I * ( t3 + t1 ) * wm * cmtops ) - cexp( -I * cmtops * ( ( wm - delta ) * t3 + ( wm * t1 ) ) );
+  f2 = exp( -g(t1) - g(t2) - g(t3) + g(t1+t2) + g(t2+t3) - g(t1+t2+t3) );
+  
+  return ( f1 * f2 );
+}
---
+++ irspec.cu
@@ -0,0 +1,408 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cufft.h>
+#include <cuComplex.h>
+
+#define nThrdsX 16
+#define nThrdsY 16
+#define nThrds 256
+
+#define clight 299792458
+#define PI 3.14159265358979323846
+#define cmtops (2*PI*clight*1.e-10)
+
+__device__ __forceinline__ cuComplex my_cexpc (cuComplex z)
+{
+
+  cuComplex res;
+
+  float t = expf(z.x);
+
+  res.x=cosf(z.y);
+  res.y=sinf(z.y);
+
+  res.x *= t;
+  res.y *= t;
+
+  return res;
+
+}
+
+__device__ __forceinline__ cuComplex my_cexpf (float z)
+{
+
+  cuComplex res;
+
+  res.x=expf(z);
+  res.y=0.f;
+
+  return res;
+
+}
+
+__device__ __forceinline__ cuComplex my_cexpi (float z)
+{
+
+  cuComplex res;
+
+  res.x=cosf(z);
+  res.y=sinf(z);
+
+  return res;
+
+}
+
+__device__ float g(float t, float* d_param)
+{
+  
+  float f1,f2,f3,f4;
+  
+  f1=d_param[3]*expf(-t/d_param[6]);
+  f2=d_param[4]*expf(-t/d_param[7]);
+  f3=d_param[5]*expf(-t/d_param[8]);
+  f4=(d_param[0]+d_param[1]+d_param[2])*t-(d_param[3]+d_param[4]+d_param[5]);
+  
+  return ( f1 + f2 + f3 + f4 );
+  
+}
+
+__device__ cuComplex Rr(float t1 ,float t2, float t3, float* d_param)
+{
+  cuComplex f1;
+  cuComplex f2;
+  
+  f1 = my_cexpi( -( t3 - t1 ) * d_param[9] * cmtops );
+  f1 = cuCsubf(f1 , cuCmulf( my_cexpi( -cmtops * ( ( d_param[9] - d_param[10] ) * t3 - ( d_param[9] * t1 ) ) ) , my_cexpf( -(d_param[12]*t3)/(2.*d_param[11]) ) ) );
+  f1 = cuCmulf(f1 , my_cexpf( -( t1 + t3 + (2.*t2) ) / ( 2.*d_param[11] ) ) );
+  f2 = my_cexpf(-g(t1,d_param) + g(t2,d_param) - g(t3,d_param) - g(t1+t2,d_param) - g(t2+t3,d_param) + g(t1+t2+t3,d_param)  );
+  
+  return cuCmulf( f1 , f2 );
+}
+
+__device__ cuComplex Rnr(float t1 ,float t2, float t3, float* d_param)
+{
+  cuComplex f1;
+  cuComplex f2;
+  
+  f1 = my_cexpi( -( t3 + t1 ) * d_param[9] * cmtops );
+  f1 = cuCsubf(f1 , cuCmulf( my_cexpi( -cmtops * ( ( d_param[9] - d_param[10] ) * t3 + ( d_param[9] * t1 ) ) ) , my_cexpf( -(d_param[12]*t3)/(2.*d_param[11]) ) ) );
+  f1 = cuCmulf(f1 , my_cexpf( -( t1 + t3 + (2.*t2) ) / ( 2.*d_param[11] ) ) );
+  f2 = my_cexpf( -g(t1,d_param) - g(t2,d_param) - g(t3,d_param) + g(t1+t2,d_param) + g(t2+t3,d_param) - g(t1+t2+t3,d_param) );
+  
+  return cuCmulf( f1 , f2 );
+}
+
+__global__ void kernelHalfFirstX(cufftComplex* d_r)
+{
+  
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  
+  cuComplex half;
+  
+  half.x=0.5f;
+  half.y=0.0f;
+  
+  d_r[i]=cuCmulf(d_r[i],half);
+  
+}
+
+__global__ void kernelHalfFirstY(cufftComplex* d_r, int n)
+{
+  
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  
+  int j = i*n;
+  
+  cuComplex half;
+  
+  half.x=0.5f;
+  half.y=0.0f;
+  
+  if (j>0)
+    d_r[j]=cuCmulf(d_r[j],half); 
+  
+}
+
+__global__ void kernelPermutation(cufftComplex* d_r,cufftComplex* d_ftr, int n)
+{
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int j = blockIdx.y * blockDim.y + threadIdx.y;
+  
+  int k = i * n + j;
+  int l = i * n + j + n/2 ;
+  int m = ( i + n/2 ) * n + j;
+  int p = ( i + n/2 ) * n + j + n/2;
+  
+  d_r[m]=d_ftr[l];
+  d_r[p]=d_ftr[k];
+  d_r[l]=d_ftr[m];
+  d_r[k]=d_ftr[p];
+}
+
+__global__ void kernelRephasing(cufftComplex* d_r,float dt,float t2,int n,float* d_param)
+{
+  
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int j = blockIdx.y * blockDim.y + threadIdx.y;
+  
+  int k = i * n + j;
+  
+  float t1 = i * dt;
+  float t3 = j * dt;
+  
+  d_r[k]=Rr(t1,t2,t3,d_param);
+  
+}
+
+__global__ void kernelNonRephasing(cufftComplex* d_r, float dt, float t2,int n,float* d_param)
+{
+  
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int j = blockIdx.y * blockDim.y + threadIdx.y;
+  
+  int k= i * n + j;
+  
+  float t1 = i * dt;
+  float t3 = j * dt;
+  
+  d_r[k]=Rnr(t1,t2,t3,d_param);
+  
+}
+
+int main(int argc, char* argv[])
+{
+  
+  FILE* input=fopen(argv[1],"r");
+  FILE* out=fopen("spec.dat","w");
+  
+  int i,ii,j,l,test;
+  int iw1,iw3,iFirst,iLast,jFirst,jLast;
+  int ndata,nw1,nw3,nave;
+  
+  float t2,dt;
+  float w1min,w1max,w3min,w3max;
+  float w1,w3;
+  float **res;
+  
+  float c1,tau1,c2,tau2,c3,tau3,wm,delta,tLife,alpha;
+  
+  fscanf(input,"%f %f %f %f",&w1min,&w1max,&w3min,&w3max);
+  fscanf(input,"%f %f %f",&delta,&tLife,&alpha);
+  fscanf(input,"%d %f",&ndata,&dt);
+  fscanf(input,"%f",&t2);
+  fscanf(input,"%d",&nave);
+  
+  ndata=(int)(ndata/nThrds)+1;
+  ndata=nThrds*2*(int)(ndata/2);
+ 
+  printf("%d %d\n",ndata,nThrds);
+ 
+  nw1=0;
+  test=1;
+  for(i=ndata/2-1;i<ndata-1;i++)
+  {
+    
+    w1=1.e10/((float)ndata*clight*dt)*(float)(1-ndata/2+i);
+    
+    if(w1>w1max)
+    {
+      iLast=i-1;
+      break;
+    }
+    
+    if( w1>=w1min )
+    {
+      nw1++;
+      
+      if(test)
+      {
+	test=0;
+	iFirst=i;
+      }
+    }
+    
+  }
+  
+  nw3=0;
+  test=1;
+  for(j=ndata/2-1;j<ndata-1;j++)
+  {
+    
+    w3=1.e10/((float)ndata*clight*dt)*(float)(1-ndata/2+j);
+    
+    if(w3>w3max)
+    {
+      jLast=j-1;
+      break;
+    }
+    
+    if( w3>=w3min )
+    {
+      nw3++;
+      
+      if(test)
+      {
+	test=0;
+	jFirst=j;
+      }
+    }
+    
+  }
+
+  printf("%d %d %d %d %d %d\n",iFirst,iLast,jFirst,jLast,nw1,nw3);
+  
+  res=(float**)malloc(nw1*sizeof(float*));
+  
+  for(i=0;i<nw1;i++)
+  {
+    res[i]=(float*)malloc(nw3*sizeof(float));
+    for(j=0;j<nw3;j++)
+      res[i][j]=0.0f;
+  }
+  
+  float *param,*d_param;
+  
+  param=(float*)malloc(13*sizeof(float));
+  cudaMalloc((void**) &d_param,13*sizeof(float));
+  
+  param[10]=delta;
+  param[11]=tLife;
+  param[12]=alpha;
+  
+  cufftComplex *d_rlist,*d_ftrlist,*rlist;
+  cufftHandle d_pr;
+  
+  rlist=(cufftComplex*)malloc(ndata*ndata*sizeof(cufftComplex));
+  
+  cudaMalloc((void**) &d_rlist,ndata*ndata*sizeof(cufftComplex));
+  cudaMalloc((void**) &d_ftrlist,ndata*ndata*sizeof(cufftComplex));
+  
+  cufftPlan2d(&d_pr,ndata,ndata,CUFFT_C2C);
+  
+  dim3 threadsPerBlock(nThrdsX, nThrdsY);
+  dim3 numBlocks(ndata / threadsPerBlock.x, ndata / threadsPerBlock.y);
+  dim3 numHalfBlocks( (ndata/2) / threadsPerBlock.x, (ndata/2) / threadsPerBlock.y );
+  
+  for(ii=0;ii<nave;ii++)
+  {
+    
+    fscanf(input,"%f",&wm);
+    fscanf(input,"%f",&c1);
+    fscanf(input,"%f",&c2);
+    fscanf(input,"%f",&c3);
+    fscanf(input,"%f",&tau1);
+    fscanf(input,"%f",&tau2);
+    fscanf(input,"%f",&tau3);
+    
+    printf("%f %f %f %f %f %f %f\n",wm,c1,c2,c3,tau1,tau2,tau3);
+
+    param[0]=c1*tau1;
+    param[1]=c2*tau2;
+    param[2]=c3*tau3;
+    
+    param[3]=param[0]*tau1;
+    param[4]=param[1]*tau2;
+    param[5]=param[2]*tau3;
+    
+    param[6]=tau1;
+    param[7]=tau2;
+    param[8]=tau3;
+    param[9]=wm;
+    
+    cudaMemcpy(d_param,param,13*sizeof(float),cudaMemcpyHostToDevice);
+    
+    // Rephasing component
+    
+    kernelRephasing<<<numBlocks,threadsPerBlock>>>(d_rlist,dt,t2,ndata,d_param);
+    kernelHalfFirstX<<<(ndata / threadsPerBlock.x),threadsPerBlock>>>(d_rlist);
+    kernelHalfFirstY<<<(ndata / threadsPerBlock.x),threadsPerBlock>>>(d_rlist,ndata);
+    
+    cufftExecC2C(d_pr,d_rlist,d_ftrlist,CUFFT_INVERSE);
+    
+    kernelPermutation<<<numHalfBlocks,threadsPerBlock>>>(d_rlist,d_ftrlist,ndata);
+    
+    cudaMemcpy(rlist,d_rlist,ndata*ndata*sizeof(cufftComplex),cudaMemcpyDeviceToHost);
+    
+    iw1=0;
+    for(i=iFirst;i<=iLast;i++)
+    {
+      iw3=0;
+      for(j=jFirst;j<=jLast;j++)
+      {
+	
+	l=ndata*(ndata-i-1)+j+1;
+	res[iw1][iw3]+=cuCrealf(rlist[l]);
+	
+	iw3++;
+      }
+      iw1++;
+    }
+    
+    // Non-rephasing component
+    
+    kernelNonRephasing<<<numBlocks,threadsPerBlock>>>(d_rlist,dt,t2,ndata,d_param);
+    kernelHalfFirstX<<<(ndata / threadsPerBlock.x),threadsPerBlock>>>(d_rlist);
+    kernelHalfFirstY<<<(ndata / threadsPerBlock.x),threadsPerBlock>>>(d_rlist,ndata);
+    
+    cufftExecC2C(d_pr,d_rlist,d_ftrlist,CUFFT_INVERSE);
+    
+    kernelPermutation<<<numHalfBlocks,threadsPerBlock>>>(d_rlist,d_ftrlist,ndata);
+    
+    cudaMemcpy(rlist,d_rlist,ndata*ndata*sizeof(cufftComplex),cudaMemcpyDeviceToHost);
+    
+    iw1=0;
+    for(i=iFirst;i<=iLast;i++)
+    {
+      iw3=0;
+      for(j=jFirst;j<=jLast;j++)
+      {
+	
+	l=ndata*(i+1)+j+1;
+	res[iw1][iw3]+=cuCrealf(rlist[l]);
+	
+	iw3++;
+      }
+      iw1++;
+    }
+  }
+  
+  cufftDestroy(d_pr);
+  
+  cudaFree(d_rlist);
+  cudaFree(d_ftrlist);
+  
+  iw1=0;
+  for(i=iFirst;i<=iLast;i++)
+  {
+    
+    w1=1.e10/((float)ndata*clight*dt)*(float)(1-ndata/2+i);
+    
+    iw3=0;
+    for(j=jFirst;j<=jLast;j++)
+    {
+      
+      w3=1.e10/((float)ndata*clight*dt)*(float)(1-ndata/2+j);
+      
+      fprintf(out,"%f\t%f\t%e\n",w1,w3,res[iw1][iw3]/(float)ndata);
+      iw3++;
+    }
+    
+    fprintf(out,"\n");
+    iw1++;
+  }
+  
+  for(i=0;i<nw1;i++)
+    free(res[i]);
+  
+  free(res);
+  
+  fclose(out);
+  fclose(input);
+  
+  return 0;
+  
+}
